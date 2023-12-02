c11b1c930645c2a903e7d7f9450293078a492e2d
excalibur-kvrv <excalibur.krv@gmail.com>
2023-12-02 10:40:51 +0530

    added skeleton

---
+++ json_parser.py
@@ -0,0 +1,69 @@
+class JSONParser:
+  def __init__(self):
+    pass
+
+  def compile_json(self):
+    pass
+
+  def compile_value(self):
+    pass
+
+  def compile_object(self):
+    pass
+
+  def compile_members(self):
+    pass
+
+  def compile_member(self):
+    pass
+
+  def compile_array(self):
+    pass
+
+  def compile_elements(self):
+    pass
+
+  def compile_element(self):
+    pass
+
+  def compile_string(self):
+    pass
+
+  def compile_characters(self):
+    pass
+
+  def compile_character(self):
+    pass
+
+  def compile_escape(self):
+    pass
+
+  def compile_hex(self):
+    pass
+
+  def compile_number(self):
+    pass
+
+  def compile_integer(self):
+    pass
+
+  def compile_digits(self):
+    pass
+
+  def compile_digit(self):
+    pass
+
+  def compile_onenine(self):
+    pass
+
+  def compile_fraction(self):
+    pass
+
+  def compile_exponent(self):
+    pass
+
+  def compile_sign(self):
+    pass
+
+  def compile_ws(self):
+    pass
---
+++ lexer.py
@@ -0,0 +1,12 @@
+class JSONTokenizer:
+  def __init__(self, file_path):
+    pass
+
+  def advance(self):
+    pass
+
+  def has_more_tokens(self):
+    pass
+
+  def current_token(self):
+    pass
